5085a1fc9929ad618ead7552154033a555060005
manipapan2 <manipapan2@gmail.com>
2022-07-18 14:09:17 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var randomizer = ['https://manipapan2.github.io/hell-or-heaven-/heaven.html' , 'https://manipapan2.github.io/hell-or-heaven-/hell.html']
 function myfunction() {
 
-    if (validation()) 
+    if (validation()) // Calling validation function
     {
+    // document.getElementById("formp").
     window.location = randomizer[Math.floor(randomizer.length*Math.random())]; // Setting form action to "success.php" page
-    }
-    else{
-     return false;
+    // document.getElementById("formp").submit(); // Submitting form
     }
 }
 
@@ -17,36 +16,30 @@ function validation() {
     var email = document.getElementById("lavak").value;
 
 
-
     if(name === 'baran' && email === 'pazoki' || name === 'mani' && email === 'papan' ||
      name === 'ilia' && email === 'akbari' || name === 'raouf' && email === 'sharifi' ||
-     name === 'farhad' && email === 'gholami' ||name === 'Baran' && email === 'Pazoki' || name === 'Mani' && email === 'Papan' ||
-     name === 'Ilia' && email === 'Akbari' || name === 'Raouf' && email === 'Sharifi' ||
-     name === 'Farhad' && email === 'Gholami'  )
+     name === 'farhad' && email === 'gholami'  )
      {
         window.location = 'https://manipapan2.github.io/hell-or-heaven-/heaven.html';
-    }
-    if(name === 'arshia' ){
-        window.location = 'https://manipapan2.github.io/hell-or-heaven-/hell.html';
-        
-    }
-    
-    if( name === 'd42MEM$%(asd3323d$1sad&@DJ' && email === 'dfj^&$(3245'){
-        window.location = 'https://manipapan2.github.io/hell-or-heaven-/crashexo.html'
-        
+        return false;
     }
 
-    else if( name === '' || email === ''){
+    if(name === 'arshia' ){
+        window.location = 'https://manipapan2.github.io/hell-or-heaven-/hell.html';
+        return false;
+    }
+
+    if( name === 'd42MEM$%(asd3323d$1sad&@DJ' && email === 'dfj^&$(3245'){
+        window.location = 'https://manipapan2.github.io/hell-or-heaven-/crashexo.html'
+        return false;
+    }
+
+
+    if( name === '' || email === ''){
         alert('pls fill the box')
         return false;
     }
+    
     else{
         window.location = randomizer[Math.floor(randomizer.length*Math.random())];
-        return true;
-    }
-
-
-
-
-
     }
